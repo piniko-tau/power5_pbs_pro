53be3d2565f9fc237f731183aaff015ed867b611
Root Power5 <root@power5.tau.ac.il>
2016-06-06 14:00:39 +0300

    new settings

--- hooks/set_guest_job_project_hook.py
+++ hooks/set_guest_job_project_hook.py
@@ -6,15 +6,20 @@ e = pbs.event()
 # Ignore requests from scheduler or server
 #if e.requestor in ["PBS_Server", "Scheduler"]:
 #	e.accept()
+if e.job.queue:
+	
+	target_qname = e.job.queue.name
+	
+	if (target_qname in ["adisR","adisE"]):
+	
+		e.job.project = "adis_guest_job_project"
+	else:	
+		e.job.project = "general_guest_job_project"
 
+else:
 
-target_qname = e.job.queue.name
+                e.job.project = "general_guest_job_project"
 
-if (target_qname in ["adisR","adisE"]):
-
-	e.job.project = "adis_guest_job_project"
-else:	
-	e.job.project = "general_guest_job_project"
-
+	
 #	e.reject("Failed to set job to project guest_job_project , queue :" + j.queue.name)
 
